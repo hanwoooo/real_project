96791e803ccfa6efbf8191d7ddf6c511fa59fa92
Hanwhy <86160798+hanwoooo@users.noreply.github.com>
2023-02-25 10:53:11 +0000

    2.25 how to use json file & study algorithm

--- Subway/views.py
+++ Subway/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 
 from .models import Subway
 from .serailizers import SubwaySerializer
+
+import json
 # Create your views here.
 
 class ListSubway(generics.ListCreateAPIView):
@@ -12,3 +14,7 @@ class ListSubway(generics.ListCreateAPIView):
 class DetailSubway(generics.RetrieveUpdateDestroyAPIView):
   queryset = Subway.objects.all()
   serializer_class = SubwaySerializer
+
+with open('./data.json', 'r') as f:
+  json_data = json.load(f)
+  print(json.dumps(json_data, indent="\t"))
